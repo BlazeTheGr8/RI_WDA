3d2510dc33eaae6a3b96851d54a9a7d1218c5bf8
Adam Castle <125893042+BlazeTheGr8@users.noreply.github.com>
2023-09-13 10:17:26 +0200

    added final touches and comments

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
+/*Import statements for importing the ExpressJS framework and the CORS middleware which handles cross-origin requests */
 const express = require('express');
 const cors = require('cors'); 
+
+// Creates an instance of the express app
 const app = express();
+
+// Defines the port number to run the server on
 const port = 3000;
 
+// Parses incoming JSON data
 app.use(express.json());
 
+// Enabled CORS for cross-origin requests
 app.use(cors());
 
+/** Defines the GET route as well as the text content being sent as a JSON response 
+ * @param {object} req - The HTTP request object.
+ * @param {object} res - The HTTP response object.
+ */
 app.get('/api/page', (req, res) => {
   const textContent = {
     registerButtonText: 'Register',
@@ -18,5 +29,5 @@ app.get('/api/page', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`); // Simple log message to show that server is running after executing 'node server.js'
 });
